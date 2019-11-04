1891ab3b0ad97a41f74f52e19b7d9504af1ff80d
OtorovDmitriy <otorov_dmitriy@iMac-Dmitrij.local>
2019-11-05 02:47:38 +0300

    ht-4 v.3

--- src/main/java/ru/sbt/mipt/oop/Main.java
+++ src/main/java/ru/sbt/mipt/oop/Main.java
@@ -1,13 +1,11 @@
 package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.sbt.mipt.oop.api.SensorEventsManagerAdapter;
 import ru.sbt.mipt.oop.event_processor.*;
 import ru.sbt.mipt.oop.file_reader.FileReader;
-import ru.sbt.mipt.oop.file_reader.FileReaderJSONStrategy;
-import ru.sbt.mipt.oop.sensor_event.SensorEvent;
 import ru.sbt.mipt.oop.sensor_event.SensorEventChangeState;
-import ru.sbt.mipt.oop.sensor_event.SensorEventLoop;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,14 +13,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String... args) throws IOException {
-        FileReaderJSONStrategy strategyJSON = new FileReaderJSONStrategy();
-        FileReader fileReader = new FileReader(strategyJSON);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        FileReader fileReader = context.getBean("fileReader", FileReader.class);
+
         SmartHome smartHome = fileReader.executeStrategy("smart-home-1.js", SmartHome.class);
 
-        EventProcessorComposite eventProcessorComposite = new EventProcessorComposite();
-        eventProcessorComposite.addProcessor(new AlarmEventProcessor());
-        eventProcessorComposite.addProcessor(new LightEventProcessor());
-        eventProcessorComposite.addProcessor(new DoorEventProcessor());
+        EventProcessorComposite eventProcessorComposite = context.getBean("eventProcessorComposite", EventProcessorComposite.class);
+        eventProcessorComposite.addProcessor(context.getBean("alarmEventProcessor", AlarmEventProcessor.class));
+        eventProcessorComposite.addProcessor(context.getBean("lightEventProcessor", LightEventProcessor.class));
+        eventProcessorComposite.addProcessor(context.getBean("doorEventProcessor", DoorEventProcessor.class));
         List<EventProcessor> processors = eventProcessorComposite.getProcessors();
 
 
